21fa97240ed84330542524523f3a8904fb1479e9
Aissatou <99013844+Aiss11@users.noreply.github.com>
2022-07-29 14:54:25 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,5 +3,5 @@
     © 2021 Copyright:
     <a class="text-dark" href="">AkatsukiTeam</a>
   </div>
-  <?php //include_once($rootPath.'/viewed_pages.php'); ?>
+  <?php //include_once($rootPath.'viewed_pages.php'); ?>
 </footer>
