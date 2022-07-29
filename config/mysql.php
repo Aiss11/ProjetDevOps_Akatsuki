4b157ffc7b8faacdab3d8f7f693c6678ec79033e
Aissatou <99013844+Aiss11@users.noreply.github.com>
2022-07-29 15:01:30 +0000

    Update mysql.php

--- config/mysql.php
+++ config/mysql.php
@@ -4,7 +4,7 @@ const MYSQL_HOST = 'localhost';
 const MYSQL_PORT = 3306;
 const MYSQL_NAME = 'we_love_food';
 const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+const MYSQL_PASSWORD = 'root';
 
 try {
     $mysqlClient = new PDO(
@@ -14,5 +14,6 @@ try {
     );
     $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $exception) {
-    die('Erreur : '.$e->getMessage());
+    die('Erreur : '.$exception->getMessage());
 }
+?>
